fa20df1ef2a909b62e81439932e28de5f5998cec
Sebastian <johancs.mm@gmail.com>
2023-08-22 22:23:08 -0500

    chore: navbar componente

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,18 @@
+import Logo from "@/components/icons/logo";
+import utils from "@/utils/index";
+import Link from "next/link";
+
 export default function Navbar() {
-  return <nav>
-    
-  </nav>;
+  return (
+    <nav className="bg-black px-5 py-2 flex items-center justify-between">
+      <Logo />
+      <ul className="flex gap-5">
+        {utils.links.map((lnk) => (
+          <li>
+            <Link href={lnk.link} className="text-white hover:underline">{lnk.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
