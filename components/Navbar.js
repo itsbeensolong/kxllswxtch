613cfb62fc07831fcb92ace901778f82e9272257
Sebastian <johancs.mm@gmail.com>
2023-08-23 21:50:08 -0500

    feat: Add about component

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,20 +4,23 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex">
-      <ul className="flex justify-center items-center gap-4 mx-4 my-6 flex-wrap">
+    <nav className="flex px-5">
+      <ul className="flex justify-center items-center gap-6 mx-4 my-6 flex-wrap">
         {utils.links.map((lnk) => (
           <li key={lnk.id}>
-            <Link href={lnk.link} className="text-white hover:underline">
+            <Link
+              href={lnk.link}
+              className="text-white hover:underline font-sans"
+            >
               {lnk.name}
             </Link>
           </li>
         ))}
       </ul>
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-6 items-center">
         {utils.media.map((md) => (
           <Link href={md.url} className="text-white">
-            <Image src={md.image} alt={md.name} className="w-auto h-7"/>
+            <Image src={md.image} alt={md.name} className="w-auto h-6" />
           </Link>
         ))}
       </div>
