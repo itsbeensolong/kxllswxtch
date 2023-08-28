2e0590b02b8332ad4df0b2cbf43c3a16bf37ca43
Sebastian <johancs.mm@gmail.com>
2023-08-27 21:27:59 -0500

    feat: Add components (#7)

--- components/works/Works.js
+++ components/works/Works.js
@@ -1,7 +1,9 @@
 export default function Works() {
   return (
-    <div>
-      <h1>Proyectos</h1>
-    </div>
+    <section className="mt-80 text-white flex items-center" id="work">
+      <h4 className="font-semibold text-2xl mb-10 hover:underline">
+        <b className="text-purple-600"># {"  "}</b>Some Projects I’ve Built
+      </h4>
+    </section>
   );
 }
