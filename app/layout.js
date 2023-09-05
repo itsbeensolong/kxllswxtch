8a5ffb6b87dce5bb77cd6be216ad3931cff0da3c
Sebastian <johancs.mm@gmail.com>
2023-09-04 21:21:40 -0500

    feat: Add component contact

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,28 @@ export default function RootLayout({ children }) {
         </div>
         <div className="bg-black">
           <main className="pl-1 lg:px-40">{children}</main>
+
+          <footer class="bg-white shadow mt-4 dark:bg-gray-800">
+            <div class="w-full mx-auto max-w-screen-xl p-2 md:flex md:items-center md:justify-between">
+              <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2023{" "}
+                <a
+                  href="https://github.com/sebastian009w"
+                  class="hover:underline"
+                >
+                  Johan Sebastian
+                </a>
+                . All Rights Reserved.
+              </span>
+              <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                  <a href="#" class="mr-4 hover:underline md:mr-6 ">
+                    Made with Next.js
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </footer>
         </div>
       </body>
     </html>
--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -1,3 +1,7 @@
+import githubImage from "@/public/github.svg";
+import linkedinImage from "@/public/linkedin.svg";
+import Image from "next/image";
+
 export default function Contact() {
   return (
     <section id="contact" className="mt-60 text-white">
@@ -5,17 +9,43 @@ export default function Contact() {
         <b className="text-purple-600"># {"  "}</b>Contacto
       </h4>
       <div className="grid grid-cols-2">
-        <article>
+        <article className="flex flex-col gap-4 items-start">
           <p>
             Estoy interesado en oportunidades de autónomos. Sin embargo, si
             tiene otra solicitud o pregunta, no dude en ponerse en contacto
             conmigo.
           </p>
+          <a
+            href="mailto:johancs.mm@gmail.com"
+            className="max-w-full p-2 bg-purple-600 mt-4 cursor-pointer active:scale-75 transition-transform"
+          >
+            Contactame!
+          </a>
         </article>
 
-        <article className="flex justify-end">
-          <div className="border border-white w-60 min-w-max rounded p-2">
-            <span>Contactame aqui</span>
+        <article className="flex justify-end items-center">
+          <div className="border border-white w-min min-w-max p-4 pt-1 flex flex-col">
+            <span>Contactame aqui!</span>
+            <div className="flex gap-4 items-center mt-4">
+              <Image
+                src={githubImage}
+                alt="Github Sebastian"
+                className="w-auto h-6"
+              />
+              <a href="#" className="hover:underline">
+                Sebastian009w
+              </a>
+            </div>
+            <div className="flex gap-4 items-center mt-4">
+              <Image
+                src={linkedinImage}
+                alt="Linked In Sebastian"
+                className="w-auto h-6"
+              />
+              <a href="#" className="hover:underline">
+                Sebastian Garcia
+              </a>
+            </div>
           </div>
         </article>
       </div>
